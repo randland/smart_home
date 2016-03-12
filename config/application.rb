64497953936c629bfd7dbfa6f6388a30814ea906
Nick Karpenske <randland@gmail.com>
2016-03-12 01:06:29 -0600

    Use sass instead of scss

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module SmartHome
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.sass.preferred_syntax = :sass
   end
 end
